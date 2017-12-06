be75662e0c47dd4bdf38a872d7beecb4e192f60a
Stargarth <adamek1221@gmail.com>
2017-12-06 14:14:53 +0100

    Consume Things

--- src/main/java/api/RestApi.java
+++ src/main/java/api/RestApi.java
@@ -3,6 +3,9 @@ package api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import datastructures.SampleClass;
 
 @RestController
 public class RestApi {
@@ -15,5 +18,13 @@ public class RestApi {
 	public String echo() {
 		
 		return "test";
+	}
+	
+	@RequestMapping("/consume")
+	public Integer consumeThing() {
+		RestTemplate restTemplate = new RestTemplate();
+        Integer status = restTemplate.getForObject("http://46.101.28.25:8080/latest", Integer.class);
+        return status;
+        
 	}
 }
