476ed3dece0ded0cdaaba7ab436fd2fc53e3d07d
Stargarth <adamek1221@gmail.com>
2017-12-06 14:38:55 +0100

    halloj2

--- src/main/java/registry/PeerRegistry.java
+++ src/main/java/registry/PeerRegistry.java
@@ -9,9 +9,9 @@ public class PeerRegistry {
 	private static List<String> registry = new ArrayList<String>();
 	
 	private PeerRegistry () {
-		registry.add("http://192.168.99.100:8080/greeting");
-		registry.add("http://192.168.99.100:8081/greeting");
-		registry.add("http://192.168.99.100:8082/greeting");
+		registry.add("http://192.168.99.100:8080");
+		registry.add("http://192.168.99.100:8081");
+		registry.add("http://192.168.99.100:8082");
 	}
 	
 	public static void addPeer(String peerAddress) {
