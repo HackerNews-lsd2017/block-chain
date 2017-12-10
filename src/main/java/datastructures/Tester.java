fdcded43323e6d4120ed0431fa00f9532aabcd24
LukaszKoziarski <lkoziars@gmail.com>
2017-12-10 19:44:31 +0100

    Added clear and simple example of a blockchain

--- src/main/java/datastructures/Tester.java
+++ src/main/java/datastructures/Tester.java
@@ -3,15 +3,49 @@ package datastructures;
 public class Tester {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		
-		Transaction t = new Transaction("Adam", 30, "YourMom");
+		Blockchain bc = new Blockchain();
+
+		// create some test transactions
+		Transaction t1 = new Transaction("Adam", 30, "YourMom");
 		Transaction t2 = new Transaction("Adam", 30, "He'sMom");
-		Block b = new Block();
-		b.addTransaction(t);
-		b.addTransaction(t2);
-		
-		
+        Transaction t3 = new Transaction("Lukasz", 50, "Adam");
+        Transaction t4 = new Transaction("Spiderman", 70, "Lukasz");
+        Transaction t5 = new Transaction("Spiderman", 70, "Batman");
+        Transaction t6 = new Transaction("Batman", 70, "YourMom");
+
+        // create blocks
+        Block b1 = new Block();
+        Block b2 = new Block();
+        Block b3 = new Block();
+
+        //  1. Add transactions to the block
+        b1.addTransaction(t1);
+        b1.addTransaction(t2);
+        //  2. Mine block
+        b1.mine(bc.getLatestBlockHash());
+        //  3. And then chain it to the blockchain
+        bc.chainBlock(b1);
+        System.out.println("---");
+
+        //  1. Add transactions to the block
+        b2.addTransaction(t3);
+        b2.addTransaction(t4);
+        //  2. Mine block
+        b2.mine(bc.getLatestBlockHash());
+        //  3. And then chain it to the blockchain
+        bc.chainBlock(b2);
+        System.out.println("---");
+
+        //  1. Add transaction to the block
+        b3.addTransaction(t5);
+        //  2. If I mine first
+        b3.mine(bc.getLatestBlockHash());
+        //  3. ... modify block and THEN try to chain it, it will not happened
+        b3.addTransaction(t6);
+        bc.chainBlock(b3);
+        System.out.println("---");
+
+        System.out.println("Latest block hash: " + bc.getLatestBlockHash());
 
 	}
 
